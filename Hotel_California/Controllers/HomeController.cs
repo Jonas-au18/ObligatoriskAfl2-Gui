60b16b1859a5b6a2f0512b107c79379cded80d41
Victor Desktop <Victo@DESKTOP-C0LLRRO>
2020-04-07 10:59:10 +0200

    add + homecontroller added kitchenview

--- Hotel_California/Controllers/HomeController.cs
+++ Hotel_California/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Hotel_California.Controllers
             return View();
         }
 
+        public IActionResult KitchenView()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
